b20546c3f4586ebf507824ec1ddb6912286f9300
Jordan Kasper <jordan@jordankasper.com>
2016-03-30 13:43:10 -0400

    Added some more code to the week 4 day 1 lecture code from day 3 discussion

--- week-4/day1/todos/js/todo-item.js
+++ week-4/day1/todos/js/todo-item.js
@@ -1,21 +1,37 @@
 
-(function(todos) {
+(function(ns) {
     'use strict';
 
     var data = [];
 
-    todos.addItem = function addItem(text) {
+    ns.addItem = function addItem(text) {
         data.push( {
             text: text,
             timestamp: Date.now()
         } );
 
-        todos.renderItem( data[data.length-1] );
+        ns.renderItem( data[data.length-1] );
+    };
+
+    var appName = 'TODO App';
+    ns.appName = appName;
+
+    ns.markItemAsCompleted = function markItemAsCompleted(todoId) {
+        // go find the data and set a "complete" switch on it
+    };
+
+    ns.removeItem = function removeItem(theIdOfTheOneToRemove) {
+        // splice the data array to take out the one you want to remove
+    };
+
+    // how many items do I have?
+    // data.length
+
+    ns.howManyItemsYouGot = function howMany() {
+        return data.length;
     };
 
 
-
-
-    window.todos = todos;
+    window.todos = ns;
 
 })(window.todos || {});
--- week-4/day1/todos/js/todo-ui.js
+++ week-4/day1/todos/js/todo-ui.js
@@ -8,10 +8,39 @@
         todos.addItem( $(this).find('input').val() );
     });
 
+    $('.items').on('click', '.complete', function doCompletion() {
+        completeItem( $(this).closest('li') );
+        todos.markItemAsCompleted( 'some way of identifying this item in the data' );
+    });
+
+    $('.items').on('click', '.delete', function doCompletion() {
+        todos.removeItem( 'the id of the item or index?' );
+    });
+
+    var itemCounter = 0;
     todos.renderItem = function renderItem(itemData) {
         $('.items')
             .append( $('<li>').text( itemData.text + ' - ' + itemData.timestamp ) );
+
+        /**
+         * if (the item is complete) {
+         *   completeItem(theElement);
+         * }
+         */
+
+        // incremenet UI counter
+        itemCounter++;
     };
+
+    // todos.appName
+
+    /**
+     * what this thing does...
+     * @param {HTMLElement} todoItem The item I want to complete
+     */
+    function completeItem(todoItem) {
+        todoItem.addClass('complete');
+    }
 
 
     window.todos = todos;
