9380aa28ee8adedbae37beb6a733be8a91b5f48f
Jordan Kasper <jordan@jordankasper.com>
2016-05-02 08:32:35 -0400

    Added stub for week 9 day 1 lecture code

---
+++ week-9/day1/app/lecture.module.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular.module('lecture', ['ui.router'])
+        .config(lectureConfig);
+
+    lectureConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function lectureConfig($stateProvider, $urlRouterProvider) {
+
+        
+
+    }
+
+})();
