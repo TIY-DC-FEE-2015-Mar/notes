9df4701f84797d77fe9aff2f3544aa45a9b4cc9f
Jordan Kasper <jordan@jordankasper.com>
2016-05-02 10:51:44 -0400

    Added first part of week 9 day 1 lecture code

--- week-9/day1/app/lecture.module.js
+++ week-9/day1/app/lecture.module.js
@@ -2,14 +2,51 @@
     'use strict';
 
     angular.module('lecture', ['ui.router'])
-        .config(lectureConfig);
+        .config(lectureConfig)
+        .run(lectureStartup);
 
     lectureConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function lectureConfig($stateProvider, $urlRouterProvider) {
 
-        
+        $urlRouterProvider.otherwise('/');
 
+        $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<p>This is home</p>'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'user/login.template.html'
+            })
+            .state('my-profile', {
+                url: '/profile',
+                templateUrl: 'secure/my-profile.template.html',
+                secure: true,
+                controller: function() {
+                    console.log('running profile controller (should not appear)');
+                }
+            });
+    }
+
+    lectureStartup.$inject = ['$rootScope', '$state'];
+    function lectureStartup($rootScope, $state) {
+        $rootScope.$on('$stateChangeStart', function checkAuth(e, toState) {
+
+            var isLoggedIn = false;  // get this from a service!!
+
+            if (toState.secure && !isLoggedIn) {
+
+                // if trying to access /foobar ... do something different
+
+                console.log('not logged in!');
+
+                e.preventDefault();
+                $state.go('login', { msg: 'hello, you are not logged in...' });
+            }
+
+        });
     }
 
 })();
